c2b13762dc3b7c45cc93d407c1127ddb5df60bd5
jasminsuljic <jasmin.suljich@gmail.com>
2020-03-18 15:20:36 +0100

    Updated release script

--- release.sh
+++ release.sh
@@ -1,8 +1,11 @@
+
 cd ..
 mkdir -p tmp/monri
 cp -r prestashop/ tmp/monri
+rm -rf tmp/monri/monri.zip
 rm -rf tmp/monri/.git
 rm -rf tmp/monri/.idea
+rm -rf tmp/monri/*.sh
 cd tmp
 zip -r monri.zip monri
 cd ..
