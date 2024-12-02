932609c5e0d835f76cce03e11172553f5126d11b
David Runtic <osdavidruntic@gmail.com>
2024-12-02 14:29:03 +0100

    Updated translations.

--- translations/hr.php
+++ translations/hr.php
@@ -2,6 +2,21 @@
 
 global $_MODULE;
 $_MODULE = array();
+$_MODULE['<{monri}prestashop>monri_c63e00415b8dfaccb614f496fe302005'] = 'Prihvatite sva plaćanja koja nudi Monri';
+$_MODULE['<{monri}prestashop>monri_876f23178c29dc2552c0b48bf23cd9bd'] = 'Jeste li sigurni da želite deinstalirati?';
+$_MODULE['<{monri}prestashop>monri_a02758d758e8bec77a33d7f392eb3f8a'] = 'Za ovaj modul nije postavljena valuta.';
+$_MODULE['<{monri}prestashop>monri_e627eca9f901e0a3b100386bee808703'] = 'Žao nam je, ovaj modul nije kompatibilan s vašom verzijom.';
+$_MODULE['<{monri}prestashop>monri_c888438d14855d7d96a2724ee9c306bd'] = 'Postavke ažurirane';
+$_MODULE['<{monri}prestashop>monri_52f4393e1b52ba63e27310ca92ba098c'] = 'Opće postavke';
+$_MODULE['<{monri}prestashop>monri_f3de88c96752f8a958ab0a01b7a4847c'] = 'Monri ključ trgovca/ID trgovine za Test';
+$_MODULE['<{monri}prestashop>monri_fbd876aba2f1cf299c71482ea8677f85'] = 'Ako ne znate svoj testni ključ trgovca, kontaktirajte support@monri.com';
+$_MODULE['<{monri}prestashop>monri_65f46baeb200259ef9a3eb1b4c811ca7'] = 'Monri trgovački ključ/ID trgovine za produkciju';
+$_MODULE['<{monri}prestashop>monri_a525ffba5468fbd4223f387d7dbca064'] = 'Ako ne znate svoj produkcijski trgovački ključ, kontaktirajte support@monri.com';
+$_MODULE['<{monri}prestashop>monri_09e3307709587fdd4e5d60725a7e3ca5'] = 'Monri token/tajna autentičnosti za testiranje';
+$_MODULE['<{monri}prestashop>monri_3af3966dea47c5db767429d4d576bf26'] = 'Ako ne znate svoj Authenticity-Token, kontaktirajte support@monri.com';
+$_MODULE['<{monri}prestashop>monri_59b73f6ef0ecc0ac80c690833dc1befe'] = 'Monri token/tajna autentičnosti za produkciju';
+$_MODULE['<{monri}prestashop>monri_384036f8b0067ec3bb710a92f8734b3b'] = 'Tip transakcije';
+$_MODULE['<{monri}prestashop>monri_92681097e087862f24282ad88296eaae'] = 'Potrebno ga je postaviti na akciju u dogovoru s Monrijem kako bi gateway ispravno funkcionirao.';
 $_MODULE['<{monri}prestashop>error_3d5d3e2e8e87468c2b91b5b3329be86d'] = 'Dogodila se pogreška';
 $_MODULE['<{monri}prestashop>error_fcb9d965230274f9cfc074abea693d02'] = 'Primijetili smo da postoji problem s vašom narudžbom.';
 $_MODULE['<{monri}prestashop>error_a588dcf8ffff5d7054eaccc848af23e8'] = 'ID košarice:';
