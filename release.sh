94888cf4bf9fcfcf1a32ffe0bee0861e90c229ca
David Runtic <osdavidruntic@gmail.com>
2024-12-10 13:34:07 +0100

    Updated release script.

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 cd ..
 mkdir -p tmp/monri
 rm -rf tmp/monri
-rsync -r --exclude '.git' --exclude 'monri.zip' --exclude '.idea' --exclude 'docker-data' --exclude '*.sh' prestashop/ tmp/monri
+rsync -r --exclude '.git' --exclude 'monri.zip' --exclude '.idea' --exclude 'docker-data' --exclude '*.sh' monri/ tmp/monri
 cd tmp
 zip -r monri.zip monri
 cd ..
-mv tmp/monri.zip prestashop/monri.zip
+mv tmp/monri.zip monri/monri.zip
