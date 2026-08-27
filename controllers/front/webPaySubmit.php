9a61eab5cfde0d15c374425983bcef0372392e82
David Runtic <osdavidruntic@gmail.com>
2026-08-27 14:01:06 +0200

    added convert to int

--- controllers/front/webPaySubmit.php
+++ controllers/front/webPaySubmit.php
@@ -36,7 +36,7 @@ class MonriwebPaySubmitModuleFrontController extends ModuleFrontController
         $cart = $this->context->cart;
         $mode = Configuration::get(MonriConstants::KEY_MODE);
         $merchant_key = Configuration::get($mode == MonriConstants::MODE_PROD ? MonriConstants::KEY_MERCHANT_KEY_PROD : MonriConstants::KEY_MERCHANT_KEY_TEST);
-        $amount = round($cart->getOrderTotal(), 2) * 100;
+        $amount = (int) round($cart->getOrderTotal() * 100);
         $form_url = $mode == MonriConstants::MODE_PROD ?
             MonriConstants::MONRI_WEBPAY_PRODUCTION_URL : MonriConstants::MONRI_WEBPAY_TEST_URL;
 
